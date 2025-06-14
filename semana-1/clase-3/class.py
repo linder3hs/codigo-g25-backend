fd0b0a5ea89f54490df48e18f997ae36eb936b87
linder3hs <linderhassinger00@gmail.com>
2025-06-13 21:48:43 -0500

    herencia

---
+++ semana-1/clase-3/class.py
@@ -0,0 +1,24 @@
+# clase o entidad
+class Auto:
+  # abstraccion, definir los atributos que van a permitir crear la clase
+  # los atributos sera recibida cundo se instancie (cree) la clase, por ende pora
+  # poder recibir los valores, vamos un constructor
+  # para crear un constructor en python, tenemos la palabra reserveda __init__
+  # una regla de las funciones dentro de una class, es que siempre el primer parametro sea self
+  # los parametros es la informacion que recibimos al instaciar la clase y esa informacion
+  # debemos guardarla dentro del contructor para poder usarla dentro de otras funciones
+  def __init__(self, marca, modelo, placa, kilometraje):
+    self.marca = marca
+    self.modelo = modelo
+    self.placa = placa
+    self.kilometraje = kilometraje
+
+  def mostrar_informacion(self):
+    print(f"Carro: {self.marca} {self.modelo}")
+
+# instancia 1
+auto_1 = Auto("Jeep", "Compass", "ABC-123", 34500)
+auto_1.mostrar_informacion()
+# instancia 2
+auto_2 = Auto("Subaru", "XRV", "ZXC-987", 1200)
+auto_2.mostrar_informacion()
---
+++ semana-1/clase-3/herencia.py
@@ -0,0 +1,61 @@
+"""
+Persona
+- nombre
+- fecha_nacimiento
+- dni
+- nacionalidad
+
+Estudiante
+- nombre
+- fecha_nacimiento
+- dni
+- nacionalidad
+- codigo
+- correo_estudiantil
+
+Empleado
+- nombre
+- fecha_nacimiento
+- dni
+- nacionalidad
+- codigo_empleado
+- correo_corporativo
+- salario
+"""
+class Persona:
+  def __init__(self, nombre, fecha_nacimiento, dni, nacionalidad):
+    self.nombre = nombre
+    self.fecha_nacimiento = fecha_nacimiento
+    self.dni = dni
+    self.nacionalidad = nacionalidad
+
+  def saludar(self):
+    print(f"Hola me llamo {self.nombre}, naci en {self.fecha_nacimiento}")
+    print(f"DNI: {self.dni}")
+    print(f"Nacionalidad: {self.nacionalidad}")
+
+# Heredar de Persona
+class Estudiante(Persona):
+  def __init__(self, nombre, fecha_nacimiento, dni, nacionalidad, codigo, correo_estudiantil):
+    # En el contructor de la clase Hija, vamos a volver a pedir los atributos
+    # para poder enviarlo a la clase padre, la forma en la que enviamos
+    # los atributos es mediante super()
+    super().__init__(nombre, fecha_nacimiento, dni, nacionalidad)
+    """
+    atributos privados: son atributos en el cual su valor solo puede ser accecido desde
+    la misma clase, es decir la instancia no puede acceder
+    la forma en la que indicamos que un atributo es privado es colocando el __ delante del nombre
+    """
+    self.__codigo = codigo
+    self.correo_estudiantil = correo_estudiantil
+
+  def mostrar_notas(self):
+    print(f"Notas del alumno: {self.nombre}")
+    print(f"Codigo: {self.__codigo}")
+    print("Nota1: 18")
+
+
+estudiante_1 = Estudiante("Pepe", "2000-10-01", "888888", "Peruano", "123456", "pepe@gmail.com")
+# print("__codigo", estudiante_1.__codigo)
+estudiante_1.saludar()
+estudiante_1.mostrar_notas()
