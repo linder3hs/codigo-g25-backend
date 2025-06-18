8776855a764ebbe0e0362aaf8025d3342b884346
linder3hs <linderhassinger00@gmail.com>
2025-06-17 21:51:56 -0500

    feat: FLASK

--- semana-2/intro-flask/app.py
+++ semana-2/intro-flask/app.py
@@ -1,5 +1,6 @@
 # importamos Flask
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from user import User
 
 # instanciar Flask
 app = Flask(__name__)
@@ -21,12 +22,46 @@ def home():
 
 @app.route("/api/v1/users")
 def get_user():
-    return "Lista de usuarios"
+    try:
+        # crear una lista para poder convertir mi
+        # arreglo de instancias a un arreglo de dicts
+        user_list = []
+
+        for user in users:
+            user_list.append(user.to_dict())
+
+        return jsonify({
+              "message:": "Lista de usuarios",
+              "users": user_list
+            })
+    except Exception as e:
+      return jsonify({
+          "error": str(e)
+        }), 500
 
 
 @app.route("/api/v1/users", methods=["POST"])
 def create_user():
-    pass
+    try:
+        data = request.get_json()
+        # creamos la instancia con la clase user
+        new_user = User(
+            data.get("name"),
+            data.get("lastname"),
+            data.get("email"),
+            data.get("password")
+        )
+        users.append(new_user)
+        return jsonify({
+            "message": "Usuario creado",
+            "user": new_user.to_dict()
+        }), 201
+    except Exception as e:
+        return jsonify({
+          "error": str(e)
+        }), 500
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- semana-2/intro-flask/user.py
+++ semana-2/intro-flask/user.py
@@ -5,5 +5,13 @@ class User:
         self.email = email
         self.password = password
 
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "lastname": self.lastname,
+            "email": self.email,
+            "password": self.password
+        }
+
     def parse_fullname(self):
         return f"{self.name} {self.lastname}"
