f0e76367f7c6faa479658afeaa2d5206dfe9dd26
linder3hs <linderhassinger00@gmail.com>
2025-07-08 22:01:15 -0500

    email service

---
+++ semana-5/services/email_service.py
@@ -0,0 +1,50 @@
+import resend
+from django.conf import settings
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class EmailService:
+
+    def __init__(self):
+        resend.api_key = settings.RESEND_API_KEY
+
+    def send_email(self, to_email, subject, html_content, text_content=None):
+        """ Function to send email using resend """
+        try:
+            if not text_content:
+                text_content = strip_tags(html_content)
+
+            params = {
+                "from": settings.DEFAULT_FROM_EMAIL,
+                "to": [to_email],
+                "subject": subject,
+                "html": html_content,
+                "text": text_content
+            }
+
+            email = resend.Emails.send(params=params)
+            logger.info(f"Email enviado correctamente a {to_email}, id: {email.get('id')}")
+            return True, email.get('id')
+        except Exception as e:
+            logger.error(f"Hubo un error en el envio de correo {e}")
+            return False, str(e)
+    
+
+    def send_verification_email(self, user, verification_token):
+        """ Email de verification """
+        verification_link = f"{settings.FRONTEND_URL}/verify-email?token={verification_token}"
+
+        context = {
+            'user': user,
+            'verifaction_link': verification_link,
+            'frontend_url': settings.FRONTEND_URL
+        }
+
+        html_content = render_to_string('email/email_verification.html', context)
+        subject = "Verifica tu email"
+
+        return self.send_email(user.email, subject, html_content)
