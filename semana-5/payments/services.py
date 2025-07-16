ae93209effbf4db0dd90c08236c91198255e9a06
linder3hs <linderhassinger00@gmail.com>
2025-07-15 22:37:20 -0500

    service mercadopago

--- semana-5/payments/services.py
+++ semana-5/payments/services.py
@@ -39,5 +39,65 @@ class MercadoPagoService:
                     "installments": 12
                 }
             }
+
+            response = self.sdk.preference().create(preference_data)
+
+            # status === 201 (created)
+            if response["status"] == 201:
+                preference = response["preference"]
+
+                return {
+                    "success": True,
+                    "preference_id": preference["id"],
+                    "init_point": preference["init_point"],
+                    "sandbox_init_point": preference["sandbox_init_point"]
+                }
+
+            return {
+                "success": False,
+                "error": "Error al crear la preferencia en Mercado Pago",
+                "detail": response
+            }
         except Exception as e:
             print(f"Error: {e}")
+            return {
+                "success": False,
+                "error": f"Error {e}",
+            }
+
+    def get_payment_info(self, payment_id):
+        """
+        Obtener el detalle de un pago
+        """
+        try:
+            response = self.sdk.payment().get(payment_id)
+            # 200 (ok)
+            if response["status"] == 200:
+                payment = response["payment"]
+                return {
+                    "success": True,
+                    "payment": {
+                        "id": payment["id"],
+                        "status": payment["status"],
+                        "status_detail": payment["status_detail"],
+                        "external_reference": payment["external_reference"],
+                        "transaction_amount": payment["transaction_amount"],
+                        "currency_id": payment["currency_id"],
+                        "payment_method_id": payment["payment_method"]["id"],
+                        "payment_type_id": payment["payment_method"]["type"],
+                        "date_created": payment["date_created"],
+                        "date_approved": payment["date_approved"],
+                        "payer_email": payment["payer"]["email"],
+                    }
+                }
+            return {
+                "success": False,
+                "error": "Error al crear la preferencia en Mercado Pago",
+                "detail": response
+            }
+        except Exception as e:
+            print(f"Error: {e}")
+            return {
+                "success": False,
+                "error": f"Error {e}",
+            }
