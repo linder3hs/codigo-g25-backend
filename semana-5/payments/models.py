bda73023add8a8663c40de89a85d551286f38629
linder3hs <linderhassinger00@gmail.com>
2025-07-15 21:21:54 -0500

    order item table

--- semana-5/payments/models.py
+++ semana-5/payments/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from products.models import Product
 
 class Order(models.Model):
   STATUS_CHOICES = [
@@ -34,3 +35,18 @@ class Order(models.Model):
   
   def __str__(self):
      return f"{self.preference_id}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = 'order_items'
+    
+    def __str__(self):
+       return f"{self.product.name} - {self.order.order_number}"
+  
