e7517b47bed608dd2529065f3c65ecbcdf6fe9e9
linder3hs <linderhassinger00@gmail.com>
2025-07-17 19:27:25 -0500

    serializers

--- semana-5/payments/serializers.py
+++ semana-5/payments/serializers.py
@@ -11,7 +11,9 @@ class CreateOrderItemSerializer(serializers.Serializer):
         Valida si el producto existe y esta activo
         """
         try:
-            Product.objects.get(id=value, is_active=True)
+            product = Product.objects.get(id=value, is_active=True)
+            if not product:
+                return serializers.ValidationError("Producto no encontrado")
             return value
         except Exception as e:
             return serializers.ValidationError("Producto no encontrado")
@@ -28,3 +30,17 @@ class CreateOrderItemSerializer(serializers.Serializer):
             return data
         except Exception:
             return serializers.ValidationError("Producto sin stock")
+
+class CreateOrderSerializer(serializers.Serializer):
+    """
+    Serializer principal para crear la orden
+    """
+    customer_email = serializers.EmailField()
+    customer_name = serializers.CharField(max_length=100)
+    customer_phone = serializers.CharField(max_length=20, required=False, allow_blank=True)
+    items = CreateOrderItemSerializer(many=True)
+
+    def validate_items(self, value):
+        if not value:
+            raise serializers.ValidationError("La orden debe tener al menos un producto.")
+        return value
