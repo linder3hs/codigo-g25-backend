21177e213fdd8f4d9f0c0b72e681d966a7aea4b7
linder3hs <linderhassinger00@gmail.com>
2025-07-24 21:17:19 -0500

    into a TS

---
+++ semana-8/ejemplo-1.ts
@@ -0,0 +1,71 @@
+// Tipos de datos?
+
+/**
+ * Tipos primitvos:
+ * string, number, boolean, null, undefined
+ */
+
+let nombre: string = "Linder";
+let numeroDeAlumnos: number = 5;
+let isActive: boolean = false;
+
+// Arreglo con multiples tipos: any
+// const nombres = [true, 10, "Linder"];
+
+const nombres: string[] = ["Pepe", "Juan", "Lucho"];
+// number[]
+const notas: Array<number> = [10.5, 11, 20, 13.4];
+
+// Podemos tipar parametros e indicar que tipo retornanos
+function sumar(n1: number, n2: number): string {
+  return `La suma es: ${n1 + n2}`;
+}
+
+sumar(10, 11);
+
+let username: string | null = null;
+username = "linder3hs";
+
+// cundo una variable o parametro tiene el simbolo "?" es opcional
+function saludar(nombre: string, edad?: number) {
+  const edadMessage = edad ? ` y tengo ${edad} años.` : ".";
+  return `Hola me llamo ${nombre}${edadMessage}`;
+}
+
+console.log(saludar("Juan"));
+console.log(saludar("Pepe", 20));
+
+interface IPerson {
+  nombre: string;
+  apellido: string;
+  direccion: string;
+  celular: number;
+  esProgramador?: boolean;
+  skills: string[];
+}
+
+const persona: IPerson = {
+  nombre: "Linder",
+  apellido: "Hassinger",
+  direccion: "Av siempre 123",
+  celular: 9999999,
+  skills: ["React", "TS", "JS", "Python"],
+};
+
+const persona2: IPerson = {
+  nombre: "Juan",
+  apellido: "Perez",
+  direccion: "Av mi casa 123",
+  celular: 888888,
+  skills: ["Java", "PHP", "Python"],
+  esProgramador: true,
+};
+
+const personas: IPerson[] = [];
+personas.push({
+  nombre: "Juan",
+  apellido: "Perez",
+  celular: 99999,
+  direccion: "av mi cas",
+  skills: ["JS", "TS"],
+});
